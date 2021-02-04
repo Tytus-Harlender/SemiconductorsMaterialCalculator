ab3a3ab1961d8ca1f094089ecbeb6ebafbd47994
Tytus Harlender <tytus.harlender95@gmail.com>
2021-02-04 14:16:01 +0100

    overwritting of Eg energy spectrum in Varshni formula modifier fixed

--- SemiconductorMaterialsCalculator/VarschniFormulaModifier.cs
+++ SemiconductorMaterialsCalculator/VarschniFormulaModifier.cs
@@ -6,15 +6,17 @@ namespace SemiconductorMaterialsCalculator
     {
         public GivenParameterSpectrum ModifyByVarschniFormula(GivenParameterSpectrum egSpectrum, double temperature)
         {
+
             var alphaSpectrum = ListOfParametersSpectrums.GetSpectrumByName("alpha");
             var bethaSpectrum = ListOfParametersSpectrums.GetSpectrumByName("betha");
-            egSpectrum.GaSb = CalculateVarschniModification(egSpectrum.GaSb,alphaSpectrum.GaSb, temperature,bethaSpectrum.GaSb);
-            egSpectrum.GaAs = CalculateVarschniModification(egSpectrum.GaAs, alphaSpectrum.GaAs, temperature,bethaSpectrum.GaAs);
-            egSpectrum.InAs = CalculateVarschniModification(egSpectrum.InAs, alphaSpectrum.InAs, temperature,bethaSpectrum.InAs);
-            egSpectrum.InSb = CalculateVarschniModification(egSpectrum.InSb, alphaSpectrum.InSb, temperature,bethaSpectrum.InSb);
-            egSpectrum.AlAs = CalculateVarschniModification(egSpectrum.AlAs, alphaSpectrum.AlAs, temperature,bethaSpectrum.AlAs);
-            egSpectrum.AlSb = CalculateVarschniModification(egSpectrum.AlSb, alphaSpectrum.AlSb, temperature, bethaSpectrum.AlSb);
-            return egSpectrum;
+            var eg1 = CalculateVarschniModification(egSpectrum.GaSb,alphaSpectrum.GaSb, temperature,bethaSpectrum.GaSb);
+            var eg2 = CalculateVarschniModification(egSpectrum.GaAs, alphaSpectrum.GaAs, temperature,bethaSpectrum.GaAs);
+            var eg3 = CalculateVarschniModification(egSpectrum.InAs, alphaSpectrum.InAs, temperature,bethaSpectrum.InAs);
+            var eg4 = CalculateVarschniModification(egSpectrum.InSb, alphaSpectrum.InSb, temperature,bethaSpectrum.InSb);
+            var eg5 = CalculateVarschniModification(egSpectrum.AlAs, alphaSpectrum.AlAs, temperature,bethaSpectrum.AlAs);
+            var eg6 = CalculateVarschniModification(egSpectrum.AlSb, alphaSpectrum.AlSb, temperature, bethaSpectrum.AlSb);
+            GivenParameterSpectrum modifiedEgSpectrum = new GivenParameterSpectrum("Eg", eg1, eg2, eg3, eg4, eg5, eg6);
+            return modifiedEgSpectrum;
         }
         public double CalculateVarschniModification(double egZeroTemperatureValue, double alphaValue, double temperature, double bethaValue)
         {
